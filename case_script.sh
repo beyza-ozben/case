345efb0a2285209236fa8ad08f07d553951f4702
beyza-ozben <21060630@stu.omu.edu.tr>
2024-08-21 14:37:21 +0300

    Cronjob çalıştı.

--- case_script.sh
+++ case_script.sh
@@ -1,14 +1,21 @@
-#!/bin/bash
 
-# Git deposunun bulunduğu dizine git
 cd /home/Masaüstü/proje/case
 
- Değişiklikleri sahnele
+sudo find /home/ -printf '%s %p\n' | sort -nr | head -1
+
+sudo find /home/ -printf '%s %p\n' | sort -nr | head -1 | awk '{ print $2 }'
+
+FILE=$(sudo find /home/ -printf '%s %p\n' | sort -nr | head -1 | awk '{ print $2 }')
+
+sha256sum "$FILE" > Big_Hash.txt
+
+if [ ! -d /home/Masaüstü/proje/case.git ]; then
+    git init
+fi
+
+# Değişiklikleri sahnele
 git add .
 
 # Commit mesajı
-git commit -m "Otomatik commit"
-
-# Değişiklikleri GitHub'a pushla
-git push origin main
+git commit -m "Cronjob çalıştı."
 
